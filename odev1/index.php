e5de833be9541d075ef28019ffe7d628f5a747b6
TOSHIBA <TOSHIBA@TOSHIBA-pc>
2014-01-28 22:29:57 +0200

    Ödev1

---
+++ odev1/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$random = rand(0,6);
+
+$fotograflar = array(
+                    '1dag_manzara.jpg',
+                    '2deniz_manzara.jpg',
+                    '3orman_manzara.jpg',
+                    '4kutuplar_manzara.jpg',
+                    '5gokyuzu_manzara.jpg',
+                    '6kuslar_manzara.jpg',
+                    '7gece_manzara.jpg'
+);
+
+
+echo $fotograflar[$random];
+
+
+
+
+
